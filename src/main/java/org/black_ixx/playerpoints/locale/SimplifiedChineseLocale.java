cafc4fc58f41d144e5e37813d15dfde9720ef18d
ahdg <36436808+ahdg6@users.noreply.github.com>
2021-04-30 21:31:51 +0800

    Update SimplifiedChineseLocale.java

--- src/main/java/org/black_ixx/playerpoints/locale/SimplifiedChineseLocale.java
+++ src/main/java/org/black_ixx/playerpoints/locale/SimplifiedChineseLocale.java
@@ -4,7 +4,7 @@ import dev.rosewood.rosegarden.locale.Locale;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class EnglishLocale implements Locale {
+public class SimplifiedChineseLocale implements Locale {
 
     @Override
     public String getLocaleName() {
