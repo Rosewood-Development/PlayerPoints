8ce8e8ab9df7a12d10b04dca4a1386b8a6cd27ce
rafael59r2 <12960698+rafael59r2@users.noreply.github.com>
2021-09-21 20:42:01 +0100

    Added PortugueseLocale (#18)

--- src/main/java/org/black_ixx/playerpoints/manager/LocaleManager.java
+++ src/main/java/org/black_ixx/playerpoints/manager/LocaleManager.java
@@ -5,12 +5,8 @@ import dev.rosewood.rosegarden.locale.Locale;
 import dev.rosewood.rosegarden.manager.AbstractLocaleManager;
 import java.util.Arrays;
 import java.util.List;
-import org.black_ixx.playerpoints.locale.EnglishLocale;
-import org.black_ixx.playerpoints.locale.FrenchLocale;
-import org.black_ixx.playerpoints.locale.JapaneseLocale;
-import org.black_ixx.playerpoints.locale.SimplifiedChineseLocale;
-import org.black_ixx.playerpoints.locale.TaiwaneseMandarinLocale;
-import org.black_ixx.playerpoints.locale.VietnameseLocale;
+
+import org.black_ixx.playerpoints.locale.*;
 
 public class LocaleManager extends AbstractLocaleManager {
 
@@ -24,6 +20,7 @@ public class LocaleManager extends AbstractLocaleManager {
                 new EnglishLocale(),
                 new FrenchLocale(),
                 new JapaneseLocale(),
+                new PortugueseLocale(),
                 new SimplifiedChineseLocale(),
                 new TaiwaneseMandarinLocale(),
                 new VietnameseLocale()
