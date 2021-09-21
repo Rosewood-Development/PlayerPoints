522b341ab3d60479553c94e61869e3fad42a838a
Esophose <esophose@gmail.com>
2021-09-21 13:45:06 -0600

    Add HungarianLocale to the list of registered locales

--- src/main/java/org/black_ixx/playerpoints/manager/LocaleManager.java
+++ src/main/java/org/black_ixx/playerpoints/manager/LocaleManager.java
@@ -5,8 +5,14 @@ import dev.rosewood.rosegarden.locale.Locale;
 import dev.rosewood.rosegarden.manager.AbstractLocaleManager;
 import java.util.Arrays;
 import java.util.List;
-
-import org.black_ixx.playerpoints.locale.*;
+import org.black_ixx.playerpoints.locale.EnglishLocale;
+import org.black_ixx.playerpoints.locale.FrenchLocale;
+import org.black_ixx.playerpoints.locale.HungarianLocale;
+import org.black_ixx.playerpoints.locale.JapaneseLocale;
+import org.black_ixx.playerpoints.locale.PortugueseLocale;
+import org.black_ixx.playerpoints.locale.SimplifiedChineseLocale;
+import org.black_ixx.playerpoints.locale.TaiwaneseMandarinLocale;
+import org.black_ixx.playerpoints.locale.VietnameseLocale;
 
 public class LocaleManager extends AbstractLocaleManager {
 
@@ -19,6 +25,7 @@ public class LocaleManager extends AbstractLocaleManager {
         return Arrays.asList(
                 new EnglishLocale(),
                 new FrenchLocale(),
+                new HungarianLocale(),
                 new JapaneseLocale(),
                 new PortugueseLocale(),
                 new SimplifiedChineseLocale(),
