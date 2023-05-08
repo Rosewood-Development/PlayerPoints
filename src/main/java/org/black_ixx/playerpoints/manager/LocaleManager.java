894d60ca8e7eaf4b4b58a528d192330aae7ada4c
Esophose <esophose@gmail.com>
2023-05-08 08:39:47 -0600

    Updated RoseGarden, fixed data not fully reloading on /points reload

--- src/main/java/org/black_ixx/playerpoints/manager/LocaleManager.java
+++ src/main/java/org/black_ixx/playerpoints/manager/LocaleManager.java
@@ -1,18 +1,7 @@
 package org.black_ixx.playerpoints.manager;
 
 import dev.rosewood.rosegarden.RosePlugin;
-import dev.rosewood.rosegarden.locale.Locale;
 import dev.rosewood.rosegarden.manager.AbstractLocaleManager;
-import java.util.Arrays;
-import java.util.List;
-import org.black_ixx.playerpoints.locale.EnglishLocale;
-import org.black_ixx.playerpoints.locale.FrenchLocale;
-import org.black_ixx.playerpoints.locale.HungarianLocale;
-import org.black_ixx.playerpoints.locale.JapaneseLocale;
-import org.black_ixx.playerpoints.locale.PortugueseLocale;
-import org.black_ixx.playerpoints.locale.SimplifiedChineseLocale;
-import org.black_ixx.playerpoints.locale.TaiwaneseMandarinLocale;
-import org.black_ixx.playerpoints.locale.VietnameseLocale;
 import org.bukkit.Bukkit;
 import org.bukkit.command.BlockCommandSender;
 import org.bukkit.command.CommandSender;
@@ -22,20 +11,6 @@ public class LocaleManager extends AbstractLocaleManager {
 
     public LocaleManager(RosePlugin rosePlugin) {
         super(rosePlugin);
-    }
-
-    @Override
-    public List<Locale> getLocales() {
-        return Arrays.asList(
-                new EnglishLocale(),
-                new FrenchLocale(),
-                new HungarianLocale(),
-                new JapaneseLocale(),
-                new PortugueseLocale(),
-                new SimplifiedChineseLocale(),
-                new TaiwaneseMandarinLocale(),
-                new VietnameseLocale()
-        );
     }
 
     public String getCurrencyName(int value) {
