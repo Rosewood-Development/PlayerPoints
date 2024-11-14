2206b3e186ed4fc2db12317d9aa4b6eb512e5c1e
Jsinco <jonahmarksinclair@gmail.com>
2024-11-14 01:08:30 -0500

    Custom command aliases

--- src/main/java/org/black_ixx/playerpoints/manager/CommandManager.java
+++ src/main/java/org/black_ixx/playerpoints/manager/CommandManager.java
@@ -5,11 +5,15 @@ import dev.rosewood.rosegarden.RosePlugin;
 import dev.rosewood.rosegarden.config.CommentedFileConfiguration;
 import dev.rosewood.rosegarden.manager.Manager;
 import java.io.File;
+import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.black_ixx.playerpoints.PlayerPoints;
 import org.black_ixx.playerpoints.commands.Commander;
+import org.black_ixx.playerpoints.setting.SettingKey;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandMap;
 import org.bukkit.command.PluginCommand;
 
 public class CommandManager extends Manager {
@@ -80,8 +84,22 @@ public class CommandManager extends Manager {
         // Register commands
         Commander commander = new Commander((PlayerPoints) this.rosePlugin);
         PluginCommand command = this.rosePlugin.getCommand("points");
-        if (command != null)
+        if (command != null) {
+
             command.setExecutor(commander);
+
+            try { // Add command aliases from config
+                Field bukkitCommandMap = Bukkit.getServer().getClass().getDeclaredField("commandMap");
+
+                bukkitCommandMap.setAccessible(true);
+                CommandMap commandMap = (CommandMap) bukkitCommandMap.get(Bukkit.getServer());
+                for (String alias : SettingKey.COMMAND_ALIASES.get()) {
+                    commandMap.register(alias, "playerpoints", command);
+                }
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
